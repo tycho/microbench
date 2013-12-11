27db63e97c0084b6e7a636c3b6d7062a8c9c2b0a
Steven Noonan <steven@uplinklabs.net>
2013-12-11 02:17:24 -0800

    DispatchLatency: initial implementation
    Very simple libdispatch latency testing in C. Similar to tools like the DPC
    Latency Checker on Windows.
    
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- libdispatch-latency/DispatchLatency/main.c
+++ libdispatch-latency/DispatchLatency/main.c
@@ -7,12 +7,74 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-int main(int argc, const char * argv[])
+#include <mach/mach_time.h>
+#include <dispatch/dispatch.h>
+
+static double clock_read_time(void)
 {
+    static mach_timebase_info_data_t timebase;
+    if (timebase.denom == 0)
+        mach_timebase_info(&timebase);
+    return (double)mach_absolute_time() * (double)timebase.numer / (double)timebase.denom;
+}
 
-    // insert code here...
-    printf("Hello, World!\n");
+static int dbl_compare(const void *a, const void *b)
+{
+    if (*(double *)a < *(double *)b) return -1;
+    if (*(double *)a > *(double *)b) return 1;
     return 0;
 }
 
+void test_dispatch_latency(unsigned samples, unsigned sleeptime)
+{
+    unsigned i;
+
+    double *lat = (double *)malloc(sizeof(double) * 3);
+    double *history = (double *)malloc(sizeof(double) * samples);
+
+    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    dispatch_group_t group = dispatch_group_create();
+
+    for (i = 0; i < samples; i++) {
+        /*
+         lat[0] to lat[1] is latency to block execution
+         lat[1] to lat[2] is latency from block execution to block synchronization
+         */
+        lat[0] = clock_read_time();
+        dispatch_group_async(group, queue, ^{ lat[1] = clock_read_time(); });
+        dispatch_group_wait(group, DISPATCH_TIME_FOREVER);
+        lat[2] = clock_read_time();
+
+        /*
+        printf("%04u lat[0] = %.0lf\n", i, lat[0]);
+        printf("%04u lat[1] = %.0lf, %.0lf\n", i, lat[1], lat[1] - lat[0]);
+        printf("%04u lat[2] = %.0lf, %.0lf\n", i, lat[2], lat[2] - lat[1]);
+        */
+
+        history[i] = lat[1] - lat[0];
+        //history[i] = lat[2] - lat[1];
+        usleep(sleeptime);
+    }
+
+    dispatch_release(group);
+
+    qsort(history, samples, sizeof(double), dbl_compare);
+
+    printf("samples = %u, sleep time = %u usecs\n", samples, sleeptime);
+    for (i = 0; i < samples; i++) {
+        printf("history[%04u] = %.0lf\n", i, history[i]);
+    }
+    printf("\n");
+
+    free(history);
+    free(lat);
+}
+
+int main(int argc, char **argv)
+{
+    test_dispatch_latency(20, 0);
+    return 0;
+}
