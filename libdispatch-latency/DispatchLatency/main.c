90c595fc3559276c920da6983557bfa8f55b8c18
Steven Noonan <steven@uplinklabs.net>
2014-01-28 04:10:46 -0800

    replace microbenchmark timers with libtime timers
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- libdispatch-latency/DispatchLatency/main.c
+++ libdispatch-latency/DispatchLatency/main.c
@@ -6,34 +6,30 @@
 //  Copyright (c) 2013 Steven Noonan. All rights reserved.
 //
 
+#include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include "libtime.h"
 
-#include <mach/mach_time.h>
 #include <dispatch/dispatch.h>
 
-static double clock_read_time(void)
-{
-    static mach_timebase_info_data_t timebase;
-    if (timebase.denom == 0)
-        mach_timebase_info(&timebase);
-    return (double)mach_absolute_time() * (double)timebase.numer / (double)timebase.denom;
-}
-
+//#define SORT
+#ifdef SORT
 static int dbl_compare(const void *a, const void *b)
 {
     if (*(double *)a < *(double *)b) return -1;
     if (*(double *)a > *(double *)b) return 1;
     return 0;
 }
+#endif
 
 void test_dispatch_latency(unsigned samples, unsigned sleeptime)
 {
     unsigned i;
 
-    double *lat = (double *)malloc(sizeof(double) * 3);
-    double *history = (double *)malloc(sizeof(double) * samples);
+    uint64_t *lat = (uint64_t *)malloc(sizeof(uint64_t) * 3);
+    uint64_t *history = (uint64_t *)malloc(sizeof(uint64_t) * samples);
 
     dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
     dispatch_group_t group = dispatch_group_create();
@@ -45,15 +41,15 @@ void test_dispatch_latency(unsigned samples, unsigned sleeptime)
          lat[0] to lat[1] is latency to block execution
          lat[1] to lat[2] is latency from block execution to block synchronization
          */
-        lat[0] = clock_read_time();
-        dispatch_group_async(group, queue, ^{ lat[1] = clock_read_time(); });
+        lat[0] = libtime_cpu();
+        dispatch_group_async(group, queue, ^{ lat[1] = libtime_cpu(); });
         dispatch_group_wait(group, DISPATCH_TIME_FOREVER);
-        lat[2] = clock_read_time();
+        lat[2] = libtime_cpu();
 
         /*
-        printf("%04u lat[0] = %.0lf\n", i, lat[0]);
-        printf("%04u lat[1] = %.0lf, %.0lf\n", i, lat[1], lat[1] - lat[0]);
-        printf("%04u lat[2] = %.0lf, %.0lf\n", i, lat[2], lat[2] - lat[1]);
+        printf("%04u lat[0] = %" PRIu64 "\n", i, lat[0]);
+        printf("%04u lat[1] = %" PRIu64 ", %" PRIu64 "\n", i, lat[1], lat[1] - lat[0]);
+        printf("%04u lat[2] = %" PRIu64 ", %" PRIu64 "\n", i, lat[2], lat[2] - lat[1]);
         */
 
         history[i] = lat[1] - lat[0];
@@ -62,11 +58,13 @@ void test_dispatch_latency(unsigned samples, unsigned sleeptime)
 
     dispatch_release(group);
 
+#ifdef SORT
     qsort(history, samples, sizeof(double), dbl_compare);
+#endif
 
     printf("samples = %u, sleep time = %u usecs\n", samples, sleeptime);
     for (i = 0; i < samples; i++) {
-        printf("history[%04u] = %.0lf\n", i, history[i]);
+        printf("history[%04u] = %" PRIu64 " nsec\n", i, libtime_cpu_to_wall(history[i]));
     }
     printf("\n");
 
@@ -76,6 +74,7 @@ void test_dispatch_latency(unsigned samples, unsigned sleeptime)
 
 int main(int argc, char **argv)
 {
+    libtime_init();
     test_dispatch_latency(10, 0);
     test_dispatch_latency(10, 1);
     test_dispatch_latency(10, 10);
--- xen-hypercall/test_vmcall.c
+++ xen-hypercall/test_vmcall.c
@@ -8,6 +8,8 @@
 
 #include <xenctrl.h>
 
+#include "libtime.h"
+
 #ifdef XENCTRL_HAS_XC_INTERFACE
 #define XC_INTERFACE_BAD_HANDLE NULL
 typedef xc_interface *xc_interface_t;
@@ -16,88 +18,6 @@ typedef xc_interface *xc_interface_t;
 #define XC_INTERFACE_BAD_HANDLE -1
 typedef int xc_interface_t;
 #endif
-
-static inline uint64_t get_cpu_clock(void)
-{
-    uint32_t lo, hi;
-
-    __asm__ __volatile__("rdtsc" : "=a" (lo), "=d" (hi));
-    return ((uint64_t) hi << 32ULL) | lo;
-}
-
-static uint32_t cycles_per_usec;
-
-static uint64_t wallclock_ns(void)
-{
-    struct timespec ts;
-    clock_gettime(CLOCK_MONOTONIC, &ts);
-    return (ts.tv_sec * 1000000000ULL) + ts.tv_nsec;
-}
-
-static uint32_t get_cycles_per_usec(void)
-{
-    uint64_t wc_s, wc_e;
-    uint64_t c_s, c_e;
-
-    wc_s = wallclock_ns();
-    c_s = get_cpu_clock();
-    do {
-        uint64_t elapsed;
-
-        wc_e = wallclock_ns();
-        elapsed = wc_e - wc_s;
-        if (elapsed >= 1280000ULL) {
-            c_e = get_cpu_clock();
-            break;
-        }
-    } while (1);
-
-    return (c_e - c_s + 127) >> 7;
-}
-
-static void calibrate_cpu_clock(void)
-{
-    const int NR_TIME_ITERS = 10;
-    double delta, mean, S;
-    uint32_t avg, cycles[NR_TIME_ITERS];
-    int i, samples;
-
-    cycles[0] = get_cycles_per_usec();
-    S = delta = mean = 0.0;
-    for (i = 0; i < NR_TIME_ITERS; i++) {
-        cycles[i] = get_cycles_per_usec();
-        delta = cycles[i] - mean;
-        if (delta) {
-            mean += delta / (i + 1.0);
-            S += delta * (cycles[i] - mean);
-        }
-    }
-
-    S = sqrt(S / (NR_TIME_ITERS - 1.0));
-
-    samples = avg = 0;
-    for (i = 0; i < NR_TIME_ITERS; i++) {
-        double this = cycles[i];
-
-        if ((fmax(this, mean) - fmin(this, mean)) > S)
-            continue;
-        samples++;
-        avg += this;
-    }
-
-    S /= (double)NR_TIME_ITERS;
-    mean /= 10.0;
-
-    avg /= samples;
-    avg = (avg + 9) / 10;
-
-    cycles_per_usec = avg;
-}
-
-uint64_t cpu_clock_to_wall(uint64_t clock)
-{
-    return (clock * 1000ULL) / cycles_per_usec;
-}
 
 int main(int argc, char **argv)
 {
@@ -111,8 +31,8 @@ int main(int argc, char **argv)
 		goto exit;
 	}
 
-	calibrate_cpu_clock();
-	s = get_cpu_clock();
+	libtime_init();
+	s = libtime_cpu();
 	for (i = 0; i < max; i++) {
 		v = xc_version(xc_handle, XENVER_version, NULL);
 		if (v < 0) {
@@ -120,10 +40,10 @@ int main(int argc, char **argv)
 			goto cleanup;
 		}
 	}
-	e = get_cpu_clock();
+	e = libtime_cpu();
 		
-	printf("total: %" PRIu64 " ns (%" PRIu64 " clocks), ", cpu_clock_to_wall(e - s), e - s);
-	printf("per call: %" PRIu64 " ns (%" PRIu64 " clocks)\n", cpu_clock_to_wall(e - s) / max, (e - s) / max);
+	printf("total: %" PRIu64 " ns (%" PRIu64 " clocks), ", libtime_cpu_to_wall(e - s), e - s);
+	printf("per call: %" PRIu64 " ns (%" PRIu64 " clocks)\n", libtime_cpu_to_wall(e - s) / max, (e - s) / max);
 	
 cleanup:
 	xc_interface_close(xc_handle);
