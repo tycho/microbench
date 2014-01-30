1c4c872bca7aa01b4822caf55e2f9bcdaaf95574
Steven Noonan <steven@uplinklabs.net>
2014-01-29 23:10:29 -0800

    hz: improve precision of rate limiting
    By estimating the minimum sleep time clock_nanosleep() will use up, we can
    determine whether or not sleeping will take us past our desired quantum. If we
    are too close to the quantum for a clock_nanosleep() to work well for us, then
    we can just spin reading the CPU clock until time runs out.
    
    For the most part, this should still be efficient, because it will sleep for
    the majority of the spare time.
    
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- hz/hz.c
+++ hz/hz.c
@@ -1,3 +1,4 @@
+#include <inttypes.h>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,26 +11,53 @@
 const char *time_suffixes[] = { "ns", "us", "ms", "s", NULL };
 const char *rate_suffixes[] = { "Hz", "KHz", "MHz", "GHz", NULL };
 
-static const char *pretty_print(char *buffer, size_t bufsz, uint64_t uv,
+static const char *pretty_print(char *buffer, size_t bufsz, double v,
 		const char **suffixes, uint32_t bar)
 {
-	double v = uv;
 	const char **suffix = suffixes;
-	while (v > bar * 1000 && suffix[1]) {
-		v /= 1000;
+	while (v > bar * 1000.0 && suffix[1]) {
+		v /= 1000.0;
 		suffix++;
 	}
-	snprintf(buffer, bufsz, "%.2lf %s", v, *suffix);
+	snprintf(buffer, bufsz, "%.3lf %s", v, *suffix);
 	return buffer;
+}
+
+uint64_t estimate_min_sleep(void)
+{
+	struct timespec ts;
+	uint32_t i, j;
+	uint64_t s, e, min;
+
+	ts.tv_sec = 0;
+	ts.tv_nsec = 0;
+
+	min = (uint64_t)-1;
+
+	for (j = 0; j < 100; j++) {
+		s = libtime_cpu();
+		for (i = 0; i < 100; i++) {
+			clock_nanosleep(CLOCK_MONOTONIC, 0, &ts, NULL);
+		}
+		e = libtime_cpu();
+		if ((e - s) < min)
+			min = (e - s);
+	}
+
+	return libtime_cpu_to_wall(min) / 100;
 }
 
 int main(int argc, char **argv)
 {
 	struct timespec ts;
-	uint64_t s, e, ns_elapsed, ns_target;
-	const uint64_t hz = 30;
-	int64_t ns_to_sleep;
+	uint64_t s, e, ns_elapsed, ns_target, tick;
+	int64_t ns_to_sleep, min_sleep;
 	char buf[2][32];
+
+	/*
+	 * The rate to enforce.
+	 */
+	const uint64_t hz = 30;
 
 	/*
 	 * Initialize our clocks.
@@ -37,10 +65,17 @@ int main(int argc, char **argv)
 	libtime_init();
 
 	/*
+	 * Find the minimum possible sleep duration.
+	 */
+	min_sleep = estimate_min_sleep();
+	printf("Estimated minimum sleep time: %s\n",
+		pretty_print(buf[0], sizeof(buf[0]), min_sleep, time_suffixes, 10));
+
+	/*
 	 * Our goal is for our iteration rate to match a certain frequency in Hz.
 	 * Find the number of expected nanoseconds per iteration.
 	 */
-	ns_target = 1e9 / (hz + 1);
+	ns_target = 1e9 / hz;
 
 	/*
 	 * Since we'll never sleep for >= 1 second, we can just initialize tv_sec
@@ -48,13 +83,16 @@ int main(int argc, char **argv)
 	 */
 	ts.tv_sec = 0;
 
+	tick = 0;
+
 	while (1) {
+		tick++;
 
 		s = libtime_cpu();
 
-		/*
-		 * Do rate-limited work here.
-		 */
+		/******************************
+		 * Do rate-limited work here. *
+		 ******************************/
 
 		e = libtime_cpu();
 
@@ -65,34 +103,50 @@ int main(int argc, char **argv)
 		ns_elapsed = libtime_cpu_to_wall(e - s);
 		ns_to_sleep = ns_target - ns_elapsed;
 
-		if (ns_to_sleep > 0) {
-			/*
-			 * We're under time, so sleep for the remainder of the
-			 * timeslice.
-			 */
-			ts.tv_nsec = ns_to_sleep;
-			clock_nanosleep(CLOCK_MONOTONIC, 0, &ts, NULL);
-		} else {
+		if (ns_elapsed > ns_target) {
 			/* 
-			 * This iteration went *over* the expected time per iteration. This
+			 * This iteration went over the expected time per iteration. This
 			 * means that our workload needs to change in order to fit the
 			 * specified frequency.
 			 */
-			printf("Completed work in %s, more than %s, not sleeping\n",
+			printf("Completed work in %s, past deadline of %s\n",
 				pretty_print(buf[0], sizeof(buf[0]), ns_elapsed, time_suffixes, 10),
 				pretty_print(buf[1], sizeof(buf[1]), ns_target, time_suffixes, 10));
 		}
 
-		/* 
-		 * Recalculate how much time we've consumed. If our sleep function is
-		 * sleeping too long or too little, this will clue us in.
-		 */
-		e = libtime_cpu();
-		ns_elapsed = libtime_cpu_to_wall(e - s);
+		while (ns_to_sleep > min_sleep) {
+			/*
+			 * We're under time, so sleep for the remainder of the
+			 * timeslice.
+			 */
+			ts.tv_nsec = 0;
+			clock_nanosleep(CLOCK_MONOTONIC, 0, &ts, NULL);
 
-		printf("Iteration completed in %s (%s)\n",
-			pretty_print(buf[0], sizeof(buf[0]), ns_elapsed, time_suffixes, 10),
-			pretty_print(buf[1], sizeof(buf[1]), 1e9 / (double)ns_elapsed, rate_suffixes, 1));
+			e = libtime_cpu();
+
+			ns_elapsed = libtime_cpu_to_wall(e - s);
+			ns_to_sleep = ns_target - ns_elapsed;
+		}
+
+		while (ns_to_sleep > 0) {
+			/*
+			 * We are very close to the time quantum, but we still have some
+			 * time to burn. Spin until we run down the clock.
+			 */
+			e = libtime_cpu();
+
+			ns_elapsed = libtime_cpu_to_wall(e - s);
+			ns_to_sleep = ns_target - ns_elapsed;
+		}
+
+		/*
+		 * Print statistics once every second.
+		 */
+		if (tick % hz == 0) {
+			printf("Iteration completed in %s (%s)\n",
+				pretty_print(buf[0], sizeof(buf[0]), ns_elapsed, time_suffixes, 10),
+				pretty_print(buf[1], sizeof(buf[1]), 1e9 / (double)ns_elapsed, rate_suffixes, 1));
+		}
 	}
 
 	return 0;
