eba092de5404281409e687960603df5e840fb601
Steven Noonan <steven@uplinklabs.net>
2014-01-29 19:10:23 -0800

    libtime: make libtime_cpu() compile under Visual C++
    Completely untested, but looks right based on documentation...
    
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- time/libtime.h
+++ time/libtime.h
@@ -1,13 +1,22 @@
 #include <stdint.h>
 #include <time.h>
 
+#ifdef _MSC_VER
+#include <intrin.h>
+#pragma intrinsic(__rdtsc)
+#endif
+
 void libtime_init(void);
 uint64_t libtime_wall(void);
 static inline uint64_t libtime_cpu(void)
 {
+#ifdef _MSC_VER
+    return __rdtsc();
+#else
     uint32_t lo, hi;
 
     __asm__ __volatile__("rdtsc" : "=a" (lo), "=d" (hi));
     return ((uint64_t) hi << 32ULL) | lo;
+#endif
 }
 uint64_t libtime_cpu_to_wall(uint64_t clock);
